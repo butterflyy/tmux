944ebef9cd146bee7d5ead4a0b86516d7e19840c
yonggang.liu <yonggang.liu@qingteng.cn>
2022-12-02 19:24:04 +0800

    22

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ cp ./twax.conf /etc/
 echo "alias tnew='twax new -s'" >> ~/.bashrc
 echo "alias tat='twax at -t'" >> ~/.bashrc
 echo "alias tls='twax ls'" >> ~/.bashrc
+echo "export TERM=xterm" >> ~/.bashrc
 source ~/.bashrc
